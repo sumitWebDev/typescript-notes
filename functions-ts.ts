6167d7d7854e797b87ea9b7021a79dc6cd0c3f28
Sumit Das <sumit.das@publicissapient.com>
2023-03-14 22:13:45 -0600

    Functions in TS

---
+++ functions-ts.ts
@@ -0,0 +1,8 @@
+function add (num1:number, num2:number){
+    return num1+num2;
+  }
+  function printResult(result):void{
+    console.log("Result is "+result)
+  }
+  printResult(add(10,20))
+  export {}
