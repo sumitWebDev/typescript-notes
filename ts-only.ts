d944158b27ccb9adad2611e939f62b4878862848
Sumit Das <dassumit.ece@gmail.com>
2023-03-19 12:44:40 -0600

    provate and public access modifiers

--- ts-only.ts
+++ ts-only.ts
@@ -1,11 +1,33 @@
 //Class name should start with Capital Letter
 class PersonalDetails{
   name: string; //this defines that class will have a name of type string
+  private employees:string[] = [];
 
   constructor(n:string){
     this.name = n
   }
+  addEmployee(employee:string){
+    this.employees.push(employee)
+  }
+  printEmployees(){
+    console.log(this.employees.length);
+    console.log(this.employees);
+
+  }
+  describe(this: PersonalDetails ){
+    console.log(this)
+    //console.log('My name'+ this.name )
+  }
 }
 
 var details = new PersonalDetails('Sumit')
-console.log(details)
+details.addEmployee('Sumit');
+details.addEmployee('Manny');
+//details.employees[2] = 'Amit'
+details.printEmployees()
+
+//details.describe();
+
+// var newDetails = {name:'Chingaa', describe:details.describe}
+// //console.log(details)
+// newDetails.describe()
