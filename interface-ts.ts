f348abfba123725557a448017b80d92c46e11669
Sumit Das <dassumit.ece@gmail.com>
2023-03-21 09:46:40 -0600

    Interface Demo

---
+++ interface-ts.ts
@@ -0,0 +1,20 @@
+interface Person{
+    name : string,
+    age: number,
+    greet() : void
+  }
+
+
+  var user1 : Person;
+
+  user1 = {
+    name:'Sumit',
+    age: 30,
+    greet(){
+      console.log()
+    }
+  }
+
+  user1.greet();
+
+
