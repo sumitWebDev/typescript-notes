3278f7665ef8026136e88238db774423f9c6b91c
Sumit Das <dassumit.ece@gmail.com>
2023-04-09 14:45:52 -0600

    Function Overload

---
+++ function-overload.ts
@@ -0,0 +1,18 @@
+
+type Combinable = string | number;
+//type Numeric = number | boolean
+
+//type Universal = Combinable & Numeric
+function add(a:string,b:string):string;
+function add(a:string,b:number):string;
+function add(a:number,b:number):number;
+function add (a:Combinable , b:Combinable){
+  if(typeof a === 'string' || typeof b === 'string' ){
+    return a.toString()+b.toString()
+  }
+return a+b
+
+}
+
+add(10,20);
+
